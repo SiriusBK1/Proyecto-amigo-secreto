b230954f19ae1363187841be5be6bc48e9c5bcf1
SiriusBK1 <aacevedovergara@gmail.com>
2025-02-23 23:20:12 -0300

    Add files via upload
    Archivos Etapa 2Ingresa amigos, muestra amigos ingresados en pantalla y elige el amigo secreto.
    Pendiente mostrar el amigo elegido.

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = [];
-// En este caso, el valor de amigo es un string, pero en el futuro podría ser un
 
 function agregarAmigo() {
-    let nuevoAmigo = document.getElementById("amigo");
+    let nuevoAmigo = document.getElementById("amigo"); //ingresa nombre en la casilla blanca
     let amigo = nuevoAmigo.value;
-    if(amigo.length > 0){
-        nuevoAmigo.value = '';
-    listaAmigos.push(amigo);
-    console.log(listaAmigos);
-    mostrarAmigos();
+    if(amigo.length > 0){     //si el nombre ingresado es mayor a 0
+        nuevoAmigo.value = ''; 
+    listaAmigos.push(amigo); // se agrega el nombre a la lista de amigos
+    console.log(listaAmigos); // se muestra en consola
         }else{
-        alert("Ingrese un nombre válido");
+        alert("Ingrese un nombre válido"); // si no se ingresa un nombre se muestra un alert
     }
+    mostrarAmigos(); //muestra los amigos en la lista
 }
 
-function mostrarAmigos() {
-    let amigos = document.getElementById("listaAmigos");
-    amigos.innerHTML = "";
+function mostrarAmigos() { //funcion para mostrar los amigos en la lista
+    let amigos = document.getElementById("listaAmigos"); //selecciona el id listaAmigos en el html
+    amigos.innerHTML = ""; 
     listaAmigos.forEach((amigo) => {
-        amigos.innerHTML += `<li>${amigo}</li>`;
+        amigos.innerHTML += `<li>${amigo}</li>`; //muestra los amigos en una lista visible en la pagina
     });
 };
 
 function elegirAmigo() {
-    let amigoElegido = Math.floor(Math.random() * listaAmigos.length)
-    console.log(listaAmigos[amigoElegido])
-    return listaAmigos[amigoElegido]
+  if(listaAmigos.length === 0){ //si la lista de amigos esta vacia
+    alert("No hay amigos para elegir"); //se muestra un alert
+    return;
+    }
+    let amigoElegido = Math.floor(Math.random() * listaAmigos.length) //elige un amigo al azar
+    console.log(listaAmigos[amigoElegido]) //muestra en consola el amigo elegido
+    return listaAmigos[amigoElegido] //retorna el amigo elegido
 }
-
+function mostrarAmigoElegido() {
+    let amigoElegidoElemento = document.getElementById("elegido"); //selecciona el id amigoElegido en el html
+    let amigo=elegirAmigo(); //se guarda el amigo elegido en una variable
+    if(amigo) {
+    amigoElegidoElemento.innerHTML = `<p>El amigo elegido es ${amigo}</p>`; //muestra el amigo elegido en la pagina
+}else{
+    alert("No hay amigos para elegir"); //si no hay amigos se muestra un alert
+    }
+}
+//mostrarAmigoElegido();//
 //function limpiarBox//
